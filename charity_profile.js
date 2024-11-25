423645e3e04ec609ff594353cb78a27af3856392
TheLoneFox1 <james.wayland@lyon.edu>
2024-11-24 21:53:14 -0600

    Update charity_profile.js

--- charity_profile.js
+++ charity_profile.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const volunteer = document.getElementById('Volunteer');
+  volunteer.addEventListener('click', function() {
+    document.getElementById('Form').style.display = 'block';
+  });
+});
 
+document.addEventListener('DOMContentLoaded', function(){
+  const Submit = document.getElementById('Submit');
+  Submit.addEventListener('click', function(){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    document.getElementById('Form').style.display = 'none';
+  });
+});
