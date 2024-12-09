7f6a063a041e58c99b91e30df61744d74b74f137
TheLoneFox1 <james.wayland@lyon.edu>
2024-12-08 22:51:18 -0600

    Update Church.js

--- Church.js
+++ Church.js
@@ -6,9 +6,9 @@ fetch('https://partners.every.org/v0.2/nonprofit/71-0239727?apiKey=pk_live_81997
         const charityName = data.data.nonprofit ? data.data.nonprofit.name : 'Not available';
         const charityLocation = data.data.nonprofit ? data.data.nonprofit.location : 'Not available';
         const charityDescritpion = data.data.nonprofit ? data.data.nonprofit.description : 'Not available';
-        document.getElementById('CharityName').innerHTML = charityName;
-        document.getElementById('Location').innerHTML = charityLocation;
-        document.getElementById('Description').innerHTML = charityDescritpion;
+        document.getElementById('CharityName1').innerHTML = charityName;
+        document.getElementById('Location1').innerHTML = charityLocation;
+        document.getElementById('Description1').innerHTML = charityDescritpion;
       })
   .catch(error => console.error('Error:', error));
 }
